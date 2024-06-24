9ec7fe66e004cb6d2bd5f7c87186ff0701fd8b38
SchunkeLisa <73402148+SchunkeLisa@users.noreply.github.com>
2024-06-24 16:02:40 +0200

    Update finetuning_script_cleaned.py
    disable flash attention

--- finetuning_script_cleaned.py
+++ finetuning_script_cleaned.py
@@ -37,7 +37,7 @@ def load_model_and_tokenizer(model_name):
     model = AutoModelForCausalLM.from_pretrained(model_name, device_map='auto',
                                                 quantization_config=bnb_config,
                                                 torch_dtype=torch.bfloat16,
-                                                attn_implementation="flash_attention_2",
+                                                #attn_implementation="flash_attention_2",
                                                 trust_remote_code=True, token=True)
 
     # Setting up the tokenizer for Phi-2
